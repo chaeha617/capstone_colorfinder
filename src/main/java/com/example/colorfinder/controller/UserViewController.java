fe9eab529229a0626c4f2a9718f767b4dce925ae
Kimju-hee <lsj240606@naver.com>
2024-06-06 13:06:14 +0900

    정보 수정 url 변경

--- src/main/java/com/example/colorfinder/controller/UserViewController.java
+++ src/main/java/com/example/colorfinder/controller/UserViewController.java
@@ -3,6 +3,9 @@ package com.example.colorfinder.controller;
 import com.example.colorfinder.entity.USERS;
 import com.example.colorfinder.service.UpdateUserRequest;
 import com.example.colorfinder.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,21 +29,39 @@ public class UserViewController {
         return "redirect:/InformationUpdate";
     }
 
-    @GetMapping("/USERS/{userId}/update")
-    public String updateUser(@PathVariable Long userId, Model model) {
+    @GetMapping("/USERS/update")
+    public String updateUser(Model model) {
+        // 현재 사용자의 인증 정보 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Principal 객체 가져오기
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        // Principal 객체에서 사용자 ID(Long) 가져오기
+        Long userId = Long.parseLong(userDetails.getUsername());
+
+        // 사용자 정보 가져오기
         USERS user = userService.getUserById(userId);
         model.addAttribute("userId", userId);
         model.addAttribute("email", user.getEmail());
         model.addAttribute("nickname", user.getNickname());
-        model.addAttribute("colorId", user.getPersonalColor().getColorId()); // 퍼스널 컬러의 이름만 가져옵니다.
-        // 이후 해당 뷰로 이동하여 사용자 정보를 표시하도록 함
+        model.addAttribute("colorId", user.getPersonalColor().getColorId());
         return "InformationUpdate";
     }
-    @PostMapping("/users/{userId}/update")
-    public String updateUser(@PathVariable Long userId, @ModelAttribute UpdateUserRequest request) {
-        userService.updateUser(userId, request);
-        return "redirect:/USERS/" + userId + "/update"; // 수정된 정보를 반영한 페이지로 리디렉션
-    }
 
+
+    @PostMapping("/users/update")
+    public String updateUser(@ModelAttribute UpdateUserRequest request) {
+        // 현재 사용자의 ID 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userIdString = authentication.getName();
+
+        // String으로 된 사용자 ID를 Long 타입으로 변환
+        Long userId = Long.parseLong(userIdString);
+
+        // 사용자 정보 업데이트
+        userService.updateUser(userId, request);
+        return "redirect:/USERS/update";
+    }
 
 }
